111bf99140f953a1f9446143f9bf84676338751f
davidjorge <davidjorge126@gmail.com>
2022-05-12 01:10:18 -0500

    :bricks: [ADD] [MOD]
    Next Pwa implementation

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
+const withPWA = require('next-pwa');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    mode: 'production',
+    disable: false,
+  },
   reactStrictMode: true,
   images: {
     domains: ['placeimg.com', 'api.lorem.space'],
   },
-};
+});
 
 module.exports = nextConfig;
